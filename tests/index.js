520d04cfa0ee3619a6b67103135a70be505b3cb3
Corey <coreybrooks2@gmail.com>
2017-05-11 16:46:55 -0500

    working on testing framework

--- tests/index.js
+++ tests/index.js
@@ -15,43 +15,22 @@ describe('/', function() {
       })
   });
 });
-describe('/beers/:category', function() {
-  it('returns an array of beers within the category', function(done) {
+describe('/all', function() {
+  it('returns all beers to help determine winning ranks', function(done) {
     chai.request(server)
-      .get('/beers/:category')
+      .get('/all')
       .end(function(err, res) {
         res.should.have.status(200);
-        done();
-      });
-  });
-  it('updates the winning beer into the database', function(done) {
-    chai.request(server)
-      .post('/beers/:id')
-      .update('content-type', 'application/json')
-      .send({name: 'Mace Windu', type: 'Master'})
-      .end(function(err, res) {
-        res.should.have.status(204);
         done();
       });
   });
 });
-//stopped about here or so
-describe('/siths', function() {
-  it('return a list of siths', function(done) {
+describe('/beers/:gen_style', function() {
+  it('returns an array of beers within the category', function(done) {
     chai.request(server)
-      .get('/siths')
+      .get('/beers/:gen_style')
       .end(function(err, res) {
         res.should.have.status(200);
-        done();
-      });
-  });
-  it('creates a new sith', function(done) {
-    chai.request(server)
-      .post('/siths')
-      .set('content-type', 'application/json')
-      .send({name: 'Darth Maul', type: 'Lord'})
-      .end(function(err, res) {
-        res.should.have.status(204);
         done();
       });
   });
